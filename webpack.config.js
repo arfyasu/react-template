82f58be84cf4002944ced23e8fc76728e7ebf522
Yasutaka Kanzaki <yasutaka.kanzaki@gmail.com>
2016-03-23 13:32:43 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  context: __dirname + "/src",
+  entry:[
+    './javascripts/app.jsx',
+    './stylesheets/app.scss'
+  ],
+
+  output: {
+    path: __dirname + '/dist/javascripts',
+    filename: 'app.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jx|jsx$/,
+        exclude: /node_modules/,
+        loader: "eslint"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css|scss$/,
+        loader: ExtractTextPlugin.extract("style", "css", "sass")
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss', '.css']
+  },
+  node: {
+    console: true
+  },
+  plugins: [
+    new ExtractTextPlugin("../stylesheets/app.css")
+  ],
+  eslint: {
+    configFile: '.eslintrc'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
